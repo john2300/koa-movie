54b9db7afff84d856997e123ed2a829831fa95cd
john2300 <1020288416@qq.com>
2019-03-23 22:00:14 +0800

    静态html页面

---
+++ server/tpl/normal.js
@@ -0,0 +1,27 @@
+module.exports = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+  <div class="contanier">
+    <div class="row">
+      <div class="col-md-8">
+        <h1>Hi Luke</h1>
+        <p>This is Scott</p>
+      </div>
+      <div class="col-md-4">
+        <p>测试静态 HTML 页面</p>
+      </div>
+    </div>
+  <div>
+</body>
+</html>
+`
